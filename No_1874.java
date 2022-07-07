047ee803038e88ecf4ec76f571b202b4e71b2757
sukkkuuuu <93762014+sukkkuuuu@users.noreply.github.com>
2022-07-07 16:21:41 +0900

    스택 수열
    스택으로 푼 문제

---
+++ No_1874.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class No_1874 {
+
+   public static void main(String[] args) throws NumberFormatException, IOException {
+      // TODO Auto-generated method stub
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+      StringBuilder sb = new StringBuilder(); // 출력할 결과를 저장 
+      
+      Stack<Integer> stack = new Stack<>();
+      
+      int N = Integer.parseInt(br.readLine()); // 첫줄 입력 받음 
+      
+      int start = 0;
+      
+      
+      while (N-->0) { // N-->0 이란 N--; 와 N>0 을 합친거랑 같음  , N 번 반복 
+         int value = Integer.parseInt(br.readLine());
+         
+         if(value > start) {
+            for (int i= start +1; i<=value; i++) { // 한 번 나온 수는 다시 스택에 들어가지 않도록 start + 1 해준다
+               stack.push(i);
+               sb.append('+').append('\n');
+            }
+            start = value; // start 값을 계속 갱신해줘서 스택에 오름차순으로 넣을 수 있는지 없는지를 판별한다
+         }
+         
+         else if (stack.peek() != value) { // 스택의 맨 위의 값(삭제하지는 않는다)과 value를 비교
+            System.out.println("NO");
+            return;
+         }
+         stack.pop(); // 마지막은 입력받은 value를 pop해준다
+         sb.append('-').append('\n');
+      }
+      System.out.println(sb);
+   }
+
+}
