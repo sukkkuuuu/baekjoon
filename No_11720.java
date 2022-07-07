71d91d05af6e123f93faea8ed957eb3edde62cc9
sukkkuuuu <93762014+sukkkuuuu@users.noreply.github.com>
2022-07-07 15:46:49 +0900

    숫자의 합
    split() 사용하여 푼 문제

---
+++ No_11720.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class No_11720 {
+
+   public static void main(String[] args) throws Exception {
+      // TODO Auto-generated method stub
+      BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+      int N = Integer.parseInt(bf.readLine());
+      
+      String[] s = bf.readLine().split(""); //split 사용하여 배열로 입력 받음
+      int sum = 0;
+      
+      for(int i=0;i<N;i++) {//배열의 합 계산
+         sum += Integer.parseInt(s[i]);
+      
+      }
+      System.out.println(sum);
+   }
+
+}
